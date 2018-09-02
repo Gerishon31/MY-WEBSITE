7713d5fa961b1f0e5c6023455dca7c843fc4c535
Gerishon <mwaniki31@gmail.com>
2018-09-02 23:56:01 +0000

    many changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function mouseOver() {
     document.getElementById("demo").style.color = "red";
+    document.getElementById("name").style.color= "pink";
 }
 
 function mouseOut() {
-    document.getElementById("demo").style.color = "black";
+    document.getElementById("demo").style.color = "green";
+    document.getElementById("name").style.color = "yellow";
 }
+
+    
+    
